02f48d3ac58718857ae45abbb3a3d2753cf4314e
Peter Breuer <peter.breuer.profiles@gmail.com>
2026-04-06 15:02:14 +0200

    feat: disable C++ and C auto-formatting

--- init.lua
+++ init.lua
@@ -699,7 +699,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = {}
+        local disable_filetypes = { cpp = true, c = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
